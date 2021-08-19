bea43a01d7d9ca0363cc31a081f7f15eb625779e
Vanderson Sampaio <vandersons.sampaio@gmail.com>
2021-08-19 20:25:52 -0300

    Adding test libraries.

--- prediction.client/src/main/java/br/ufsc/ine/ppgcc/client/MachineLearningClient.java
+++ prediction.client/src/main/java/br/ufsc/ine/ppgcc/client/MachineLearningClient.java
@@ -1,6 +1,6 @@
 package br.ufsc.ine.ppgcc.client;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
